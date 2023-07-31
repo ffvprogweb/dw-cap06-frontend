b7b22e79c4a5c4830d009468d3924044bc5de28c
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2023-07-31 14:12:00 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Home from "./Home";
+import "./styles.css";
+import ConsultaCatalogo from "./componentes/ConsultaCatalogo";
+import CadastrarProduto from "./componentes/CadastrarProduto";
+import UploadImagem from "./componentes/UploadImagem";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/produtos/catalogo" element={<ConsultaCatalogo />} />
+        <Route path="/produtos/cadastrar" element={<CadastrarProduto />} />
+        <Route path="/produtos/upload" element={<UploadImagem />} />
+      </Routes>
+    </Router>
+  );
+};
+export default App;
---
+++ src/componentes/ConsultaCatalogo.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ConverteBase64ToImage from "./ConverteBase64ToImage";
+import "./styles.css";
+
+const ConsultaCatalogo = () => {
+  const [produtos, setProdutos] = useState([]);
+  const [erro, setErro] = useState(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const consulta = async () => {
+      try {
+        const resposta = await fetch("http://localhost:8080/api/v1/produtos");
+        if (!resposta.ok) {
+          throw new Error();
+        }
+        const dados = await resposta.json(); //retorna um array de objetos json
+
+        //carrega os dados na variavel produto
+        setProdutos(dados);
+      } catch (error) {
+        setErro(error.message);
+      }
+    };
+    consulta();
+  }, []);
+  const handleVoltar = () => {
+    // Navegar para a rota "/home"
+    navigate("/home");
+  };
+
+  return (
+    <div>
+      <head>
+        <title>
+          <h3> Consulta Catalogo </h3>
+        </title>
+      </head>
+      <table id="produtos">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Descrição</th>
+            <th>Custo</th>
+            <th>Quant</th>
+            <th>Peça</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.id}</td>
+              <td>{produto.descricao}</td>
+              <td>{produto.custo}</td>
+              <td>{produto.quantidade}</td>
+              <td>
+                <img
+                  src={ConverteBase64ToImage(produto.imagem)}
+                  alt="Imagem "
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p></p>
+      <button onClick={handleVoltar}>Voltar</button>
+      <p></p>
+      {erro && (
+        <>
+          <p>Erro na consulta: {erro} </p>{" "}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ConsultaCatalogo;
